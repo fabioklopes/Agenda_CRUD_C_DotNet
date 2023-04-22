856260970cdaa9cb91b39e06f517761a47a1dd0c
fabio <fabio@DESKTOP-77C6DOP>
2023-04-22 13:51:35 -0300

    Adicionar arquivos de projeto.

---
+++ Contact/Principal.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Logotipo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Desenvolvido por: FKL Digital Conenct\n\n" +
+                "Versão: 1.0.0", "Sobre...");
+        }
+
+        private void peopleBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.peopleBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.contatoDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'contatoDBDataSet.people'. Você pode movê-la ou removê-la conforme necessário.
+            this.peopleTableAdapter.Fill(this.contatoDBDataSet.people);
+
+        }
+    }
+}
---
+++ Contact/dbo.People.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[people]
+(
+	[ContactId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(100) NOT NULL, 
+    [Company] VARCHAR(100) NULL, 
+    [Telephone] VARCHAR(25) NULL,
+	[Email] VARCHAR(100) NULL, 
+    [Client] BIT NULL, 
+    [LastCall] DATETIME NULL    
+)
